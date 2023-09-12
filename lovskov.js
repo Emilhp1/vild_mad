789dbd10329a6f21d8b888fa85ed44abfbf64694
Liv <livbendix@live.dk>
2023-09-12 15:31:27 +0200

    allan

--- lovskov.js
+++ lovskov.js
@@ -25,15 +25,7 @@ function showCategory(category) {
 const url = "https://ebwnpfllzucpiixhtnhu.supabase.co";
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVid25wZmxsenVjcGlpeGh0bmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5MTIzMDEsImV4cCI6MjAwOTQ4ODMwMX0.CyZqh6CsJ8T2r-1KUO0oillkNqf7941nqLFY3jCnbSA";
 
-// Create a mapping between numeric month values and month names
-const monthNameMapping = {
-  1: "Forår",
-  2: "Sommer",
-  3: "Efterår",
-  4: "Vinter",
-};
-
-fetch(url + "/rest/v1/vildmad3", {
+fetch(url + "/rest/v1/seasons", {
   method: "GET",
   headers: {
     apikey: key, // Use lowercase "apikey" here
@@ -41,33 +33,19 @@ fetch(url + "/rest/v1/vildmad3", {
 })
   .then((res) => res.json())
   .then((data) => {
-    // Call the showMonths function with the data
-    showMonths(data);
+    // Call the showData function with the data
+    showData(data);
   });
 
-function showMonths(monthsData) {
+function showData(data) {
+  console.log(data);
   const gridKat = document.querySelector(".grid_kat");
 
-  // Create an array to keep track of displayed months
-  const displayedMonths = [];
-
-  monthsData.forEach((monthItem) => {
+  data.forEach((item) => {
     const template = document.querySelector("template").content;
     const copy = template.cloneNode(true);
-
-    // Assuming your Supabase data has a "month" field with numeric values
-    const numericMonth = monthItem.month; // Replace "month" with the actual field name
-
-    // Get the corresponding month name from the mapping
-    const monthName = monthNameMapping[numericMonth];
-
-    // Check if this month has already been displayed
-    if (!displayedMonths.includes(monthName)) {
-      copy.querySelector("a").textContent = monthName;
-      copy.querySelector("a").setAttribute("href", `måned.html?season=${monthName}`); // Update the URL as needed
-
-      gridKat.appendChild(copy);
-      displayedMonths.push(monthName); // Mark this month as displayed
-    }
+    copy.querySelector("div").textContent = item.navn;
+    copy.querySelector("a").setAttribute("href", `måned.html?season=${item.id}&navn=${item.navn}`); // Update the URL as needed
+    gridKat.appendChild(copy);
   });
 }
