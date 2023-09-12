235ddd305fd7641931c67eaa9a0e69a0a5b4232c
Emilhp1 <124664688+Emilhp1@users.noreply.github.com>
2023-09-12 15:36:22 +0200

    s

--- måned.js
+++ måned.js
@@ -1,6 +1,6 @@
 const id = urlParams.get("liv");
 
-fetch("https://ebwnpfllzucpiixhtnhu.supabase.co/rest/v1/vildmad3?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVid25wZmxsenVjcGlpeGh0bmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5MTIzMDEsImV4cCI6MjAwOTQ4ODMwMX0.CyZqh6CsJ8T2r-1KUO0oillkNqf7941nqLFY3jCnbSA&select=month" + id)
+fetch("https://ebwnpfllzucpiixhtnhu.supabase.co/rest/v1/vildmad3?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVid25wZmxsenVjcGlpeGh0bmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5MTIzMDEsImV4cCI6MjAwOTQ4ODMwMX0.CyZqh6CsJ8T2r-1KUO0oillkNqf7941nqLFY3jCnbSA&select=title" + id)
   .then((res) => res.json())
   .then(showProducts);
 
