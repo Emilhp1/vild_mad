f77a88470e1b6e2360fd4b2a07c9ce940ed7062b
“Kezia <keha0001@stud.kea.dk>
2023-09-13 16:07:22 +0200

    Få forbundet js, tilføjet billeder og css

--- produkt.js
+++ produkt.js
@@ -1,25 +1,4 @@
-// const urlParams = new URLSearchParams(window.location.search);
-// const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVid25wZmxsenVjcGlpeGh0bmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5MTIzMDEsImV4cCI6MjAwOTQ4ODMwMX0.CyZqh6CsJ8T2r-1KUO0oillkNqf7941nqLFY3jCnbSA";
-// const id = urlParams.get("id");
-
-// const options = {
-//   method: "GET",
-//   headers: {
-//     apikey: key,
-//   },
-// };
-
-// fetch(`https://ebwnpfllzucpiixhtnhu.supabase.co/rest/v1/vildmad3?id=eq.${id}`, options)
-//   .then((res) => res.json())
-//   .then((data) => showProduct(data));
-
-// function showProduct(product) {
-//   console.log(product);
-//   document.querySelector(".overskrift").textContent = product.title;
-//   document.querySelector(".card img").src = product.profile_image;
-//   document.querySelector(".card .bio").textContent = product.bio;
-// }
-
+/* Tilføjet af kezia - Laver kommentarene senere */
 document.addEventListener("DOMContentLoaded", function () {
   const urlParams = new URLSearchParams(window.location.search);
   const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVid25wZmxsenVjcGlpeGh0bmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5MTIzMDEsImV4cCI6MjAwOTQ4ODMwMX0.CyZqh6CsJ8T2r-1KUO0oillkNqf7941nqLFY3jCnbSA";
@@ -44,29 +23,55 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".sankested").textContent = product[0].sankested;
     document.querySelector(".sæson").textContent = product[0].årstider;
     document.querySelector(".beskrivelse").textContent = product[0].beskrivelse;
-    document.querySelector(".sankning").textContent = product[0].beskrivelse;
+    document.querySelector(".sankning").textContent = product[0].sankning;
     document.querySelector(".nb").textContent = product[0].nb;
+    // Select the .nb paragraph
+    const nbParagraph = document.querySelector(".nb");
+
+    // Check if the .nb paragraph has content
+    if (nbParagraph.textContent.trim().length === 0) {
+      // If it has no content, hide the associated icon
+      const nbIcon = nbParagraph.previousElementSibling;
+      if (nbIcon && nbIcon.classList.contains("icon")) {
+        nbIcon.style.display = "none";
+      }
+    }
     document.querySelector(".risiko").textContent = product[0].risiko;
+
+    /* Tilføjet af kezia - ignorer den her del for nu */
+    /*   const risikoParagraph = document.querySelector(".risiko");
+
+    // Check if the .risiko paragraph contains "Ingen risiko for forveksling"
+    if (!risikoParagraph.textContent.trim().length === 0) {
+      // If there's no risk, hide the associated icon
+      const risikoIcon = risikoParagraph.previousElementSibling;
+      if (risikoIcon && risikoIcon.classList.contains(".speech-bubble")) {
+        risikoIcon.style.display = "none";
+      }
+    } */
 
     // Select the risk element by its class
     const riskElement = document.querySelector(".risiko");
+    // Set the speech bubble arrow color to red
+    const speechBubbleArrow = document.querySelector(".speech-bubble");
+
+    // Check the content of the risk element
     if (riskElement.textContent.includes("Ingen risiko for forveksling")) {
       // If it contains "ingen risiko," set the background color to green
       riskElement.style.backgroundColor = "#46C34A";
 
       // Set the speech bubble arrow color to green
-      const speechBubbleArrow = document.querySelector(".speech-bubble:after");
-      speechBubbleArrow.style.borderTopColor = "#BCFFC2";
+      speechBubbleArrow.style.borderTopColor = "#46C34A";
     } else {
       // Otherwise (if it doesn't contain "ingen risiko"), set the background color to red
-      riskElement.style.backgroundColor = "#FF3C2C";
+      riskElement.style.backgroundColor = "red";
+      speechBubbleArrow.style.borderTopColor = "red";
     }
-
     document.querySelector(".håndtering").textContent = product[0].håndtering;
     document.querySelector(".anvendelse").textContent = product[0].anvendelse;
     document.querySelector(".opbevaring").textContent = product[0].opbevaring;
     document.querySelector(".erstatningsmuligheder").textContent = product[0].erstatningsmuligheder;
-    document.querySelector(".mund").textContent = product[0]["i munden"];
-    document.querySelector(".næse").textContent = product[0]["i næsen"];
+    document.querySelector(".mund-text").textContent = product[0]["i munden"];
+    document.querySelector(".næse-text").textContent = product[0]["i næsen"];
   }
 });
